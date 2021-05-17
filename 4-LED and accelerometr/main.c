16d6a1522b36fd72a27f57526c42a032c2fe73f6
Aleksey Golovenchik <59099639+AlGol86@users.noreply.github.com>
2021-05-17 14:13:47 +0300

    Update main.c

--- 4-LED and accelerometr/main.c
+++ 4-LED and accelerometr/main.c
@@ -1,1 +1,24 @@
+#include "led.h"
+#include "iic.h"
+#include "gy_521.h"
+
+
+
+int main( void )
+{
+ supply ();
+ initGY_521();
+ init_LED();
+ 
+ 
+ while(1){
+ int x=get_average_GY_521(AXEL_X);
+  x/=10;
+ printValue((int long)x, 0); 
+ }
+
+
+}
+  
+
 
